42d88444a51f9c3e3b8c1070939365f986e9da82
jordan <jharrison1@qa.com>
2022-07-26 09:52:47 +0100

    Encapsulated my dogs

--- src/com/qa/objects/Dog.java
+++ src/com/qa/objects/Dog.java
@@ -3,10 +3,10 @@ package com.qa.objects;
 public class Dog {
 
 	// instance variables
-	String name = "Woofy";
-	String breed;
-	int age;
-	String colour;
+	private String name = "Woofy";
+	private String breed;
+	private int age;
+	private String colour;
 
 	public Dog() {
 	}
@@ -29,4 +29,40 @@ public class Dog {
 		System.out.println("I am " + this.age + " years old!");
 		System.out.println("And I am a " + colour + " " + breed);
 	}
+
+	public void setAge(int age) {
+		if (age > 0 && age < 20)
+			this.age = age;
+		else
+			System.out.println("Invalid age: " + age);
+	}
+
+	public int getAge() {
+		return this.age;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBreed() {
+		return this.breed;
+	}
+
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+
+	public String getColour() {
+		return this.colour;
+	}
+
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+
 }
--- src/com/qa/objects/Runner.java
+++ src/com/qa/objects/Runner.java
@@ -7,34 +7,23 @@ public class Runner {
 		Integer i2 = 46;
 
 		Dog dog1 = new Dog();
-		dog1.name = "Jamal";
-		dog1.breed = "Bulldog";
-		dog1.age = 12;
-		dog1.colour = "White";
+//		dog1.name = "Jamal";
+//		dog1.breed = "Bulldog";
+//		dog1.age = 12;
+//		dog1.colour = "White";
 		dog1.print();
 
 		Dog dog2 = new Dog();
-		dog2.name = "Rex";
-		dog2.breed = "German Shepherd";
-		dog2.age = 6;
-		dog2.colour = "Brown";
-		dog2.print();
+//		dog2.name = "Rex";
+//		dog2.breed = "German Shepherd";
+//		dog2.age = 6;
+//		dog2.colour = "Brown";
+//		dog2.print();
 
-		Dog dog3 = makeDog("Woofy", "Doberman", 3, "Black");
-		dog3.print();
-
-		System.out.println(dog3);
-
-		Dog dog4 = new Dog("Fido", "Dalmation", 12, "Black and white");
-	}
-
-	public static Dog makeDog(String name, String breed, int age, String colour) {
-		Dog d = new Dog();
-		d.name = name;
-		d.breed = breed;
-		d.age = age;
-		d.colour = colour;
-		return d;
+		Dog dog5 = new Dog("Barry", "Bulldog", 6, "Brown");
+//		dog5.age = Integer.MAX_VALUE; can't access as it is now private
+		dog5.setAge(Integer.MAX_VALUE);
+		dog5.print();
 	}
 
 }
